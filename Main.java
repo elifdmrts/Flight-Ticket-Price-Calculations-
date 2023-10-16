9ac778ef2643d49c0c68bb9285c1b4af5bfff9a3
Elif Demirtaş <elif_demirtas_@outlook.com>
2023-10-16 18:55:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double mesafe, fıyat, yasIndrm, tıpIndırım;
+        int yas, yolculukTıpı;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Mesafeyi KM Cinsinden Giriniz: ");
+        mesafe = scanner.nextDouble();
+        System.out.print("Yaşınızı Giriniz: ");
+        yas = scanner.nextInt();
+        System.out.println("Yolculuk Tipini Seçiniz:\n1-)Tek Yön\n2-)Gidiş-Dönüş");
+        yolculukTıpı = scanner.nextInt();
+        fıyat = mesafe * 0.10;
+
+        if (yas < 12) {
+            yasIndrm = (fıyat) * (0.5);
+            fıyat = fıyat - yasIndrm;
+            // System.out.println("Fiyat: " + fıyat);
+        } else if (yas > 12 && yas < 24) {
+            yasIndrm = (fıyat) * (0.1);
+            fıyat = fıyat - yasIndrm;
+            //System.out.println("Fiyat: " + fıyat);
+        } else if (yas > 65) {
+            yasIndrm = (fıyat) * (0.3);
+            fıyat = fıyat - yasIndrm;
+            // System.out.println("Fiyat: " + fıyat);
+        }
+
+        if (yolculukTıpı == 2) {
+            tıpIndırım = fıyat * (0.2);
+            fıyat = fıyat - tıpIndırım;
+            System.out.println("Toplam Tutar: " + fıyat + "TL");
+
+        } else if (yolculukTıpı != 1 && yolculukTıpı != 2) {
+            System.out.println("Hatalı Veri Girdiniz");
+        } else System.out.println("Toplam Tutar: " + fıyat + "TL");
+    }
+}
